1cd985fea10bdcd9ebb27567894b6654765395c7
eedrxs <alabialade@gmail.com>
2022-10-28 12:28:40 +0100

    CORS issue fix attempt

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express")
-const dotenv = require("dotenv")
-
-dotenv.config()
+const dotenv = require("dotenv").config()
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 const slackUsername = 'eedris'
 const backend = true
 const age = 22
 const bio = 'Aspiring fullstack developer.'
 
-app.get("/", (req, res) => res.json({ slackUsername, backend, age, bio }))
+app.get("/", (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.status(200).json({ slackUsername, backend, age, bio })
+})
 
 app.listen(port, () => console.log(`[server]: Server is running at https://localhost:${port}`))
